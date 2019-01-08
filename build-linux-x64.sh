daef3612ace8d95083b5955812dc4443fc220e1b
Dirk Raffel <dirk.raffel@gmail.com>
2019-01-08 17:50:54 +0100

    edit release

--- build-linux-x64.sh
+++ build-linux-x64.sh
@@ -2,6 +2,6 @@
 rm -f tailon
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 make
 tar zcvf tailon_linux_amd64.tar.gz tailon LICENSE README.md frontend/dist frontend/templates
-hub release create -a tailon_linux_amd64.tar.gz v1.0.0
-# hub release edit -a tailon_linux_amd64.tar.gz v1.0.0
+# hub release create -a tailon_linux_amd64.tar.gz v1.0.0
+hub release edit -a tailon_linux_amd64.tar.gz v1.0.0
 
